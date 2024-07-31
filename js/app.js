ad333bb5bb09eae824870bb6eaa58af9f74b65a8
jborruel308@me.com <jborruel308@me.com>
2024-07-30 19:20:16 -0700

    step 1 pass

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+// Our global state object 
+let state = {
+    boredem: 0, 
+    hunger: 0, 
+    sleepiness: 0,
+};
+
+let timer, gameOver; //This will assign the timer and gameOVer a value of undefined
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
